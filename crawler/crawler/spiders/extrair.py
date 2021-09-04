5f00a78dfdb7c9c1e60fe5a47f7c828ae8fef2aa
crissipires <crissiano_pire@hotmail.com>
2021-09-04 15:57:18 -0300

    :white_check_mark: somes update, all ok!

--- crawler/crawler/spiders/extrair.py
+++ crawler/crawler/spiders/extrair.py
@@ -1,27 +1,26 @@
 import scrapy
 from crawler.items import NewsItens
 
+
 class ExtrairSpider(scrapy.Spider):
     name = 'quotes'
-
-    def start_requests(self):
-        urls = 'http://noosfero.ucsal.br/institucional/noticias/'
-        yield scrapy.Request(url=urls, callback=self.parse)
+    start_urls = ['http://noosfero.ucsal.br/institucional/noticias/',]
 
     def parse(self, response):
-        global notice
+        global news
         for post in response.css("div.blog-post"):
             title = post.xpath('*//h1[@class="title"]/a/text()').get()
             date = post.xpath('*//span[@class="date"]/text()').get()
             link = post.xpath('*//h1[@class="title"]/a/@href').get()
 
-            notice = NewsItens(title=title,date=date,link=link)
-        yield notice
+            news = NewsItens(title=title, date=date, link=link)
+
+        yield news
 
         next_page = response.xpath("*//a[@class='next_page']/@href").get()
 
         if next_page is not None:
-            yield scrapy.Request(response.urljoin(next_page),callback=self.parse)
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
 
 
